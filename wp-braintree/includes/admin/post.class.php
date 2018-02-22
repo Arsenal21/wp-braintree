62e3ab18aaaff4dd998fa3c56d52fb4bbaef6927
erommel <eminemwalsh@gmail.com>
2018-02-22 13:51:31 +0200

    Reformatted code

--- wp-braintree/includes/admin/post.class.php
+++ wp-braintree/includes/admin/post.class.php
@@ -3,80 +3,80 @@
 class wp_braintree_post {
 
     public function __construct() {
-        
+	
     }
 
     static function register_post_type() {
-        $labels = array(
-            'name' => _x('Payments', 'Post Type General Name', 'wp_braintree_lang'),
-            'singular_name' => _x('Payment', 'Post Type Singular Name', 'wp_braintree_lang'),
-            'menu_name' => __('WP Braintree', 'wp_braintree_lang'),
-            'parent_item_colon' => __('Parent Payment:', 'wp_braintree_lang'),
-            'all_items' => __('Payments', 'wp_braintree_lang'),
-            'view_item' => __('View Payment', 'wp_braintree_lang'),
-            'add_new_item' => __('Add New Payment', 'wp_braintree_lang'),
-            'add_new' => __('Add New', 'wp_braintree_lang'),
-            'edit_item' => __('Edit Payment', 'wp_braintree_lang'),
-            'update_item' => __('Update Payment', 'wp_braintree_lang'),
-            'search_items' => __('Search Payment', 'wp_braintree_lang'),
-            'not_found' => __('Not found', 'wp_braintree_lang'),
-            'not_found_in_trash' => __('Not found in Trash', 'wp_braintree_lang'),
-        );
+	$labels = array(
+	    'name'			 => _x( 'Payments', 'Post Type General Name', 'wp_braintree_lang' ),
+	    'singular_name'		 => _x( 'Payment', 'Post Type Singular Name', 'wp_braintree_lang' ),
+	    'menu_name'		 => __( 'WP Braintree', 'wp_braintree_lang' ),
+	    'parent_item_colon'	 => __( 'Parent Payment:', 'wp_braintree_lang' ),
+	    'all_items'		 => __( 'Payments', 'wp_braintree_lang' ),
+	    'view_item'		 => __( 'View Payment', 'wp_braintree_lang' ),
+	    'add_new_item'		 => __( 'Add New Payment', 'wp_braintree_lang' ),
+	    'add_new'		 => __( 'Add New', 'wp_braintree_lang' ),
+	    'edit_item'		 => __( 'Edit Payment', 'wp_braintree_lang' ),
+	    'update_item'		 => __( 'Update Payment', 'wp_braintree_lang' ),
+	    'search_items'		 => __( 'Search Payment', 'wp_braintree_lang' ),
+	    'not_found'		 => __( 'Not found', 'wp_braintree_lang' ),
+	    'not_found_in_trash'	 => __( 'Not found in Trash', 'wp_braintree_lang' ),
+	);
 
-        $menu_icon = WPB_URL . '/js/images/wp_braintree.png';
-        $args = array(
-            'label' => __('payments', 'wp_braintree_lang'),
-            'description' => __('WP Braintree', 'wp_braintree_lang'),
-            'labels' => $labels,
-            'supports' => array('title', 'editor', 'excerpt', 'revisions', 'custom-fields',),
-            'hierarchical' => false,
-            'public' => false,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => true,
-            'show_in_admin_bar' => true,
-            'menu_position' => 80,
-            'menu_icon' => $menu_icon,
-            'can_export' => true,
-            'has_archive' => false,
-            'exclude_from_search' => true,
-            'publicly_queryable' => false,
-            'capability_type' => 'post',
-            'capabilities' => array(
-                'create_posts' => false, // Removes support for the "Add New" function
-            ),
-            'map_meta_cap' => true,
-        );
+	$menu_icon	 = WPB_URL . '/js/images/wp_braintree.png';
+	$args		 = array(
+	    'label'			 => __( 'payments', 'wp_braintree_lang' ),
+	    'description'		 => __( 'WP Braintree', 'wp_braintree_lang' ),
+	    'labels'		 => $labels,
+	    'supports'		 => array( 'title', 'editor', 'excerpt', 'revisions', 'custom-fields', ),
+	    'hierarchical'		 => false,
+	    'public'		 => false,
+	    'show_ui'		 => true,
+	    'show_in_menu'		 => true,
+	    'show_in_nav_menus'	 => true,
+	    'show_in_admin_bar'	 => true,
+	    'menu_position'		 => 80,
+	    'menu_icon'		 => $menu_icon,
+	    'can_export'		 => true,
+	    'has_archive'		 => false,
+	    'exclude_from_search'	 => true,
+	    'publicly_queryable'	 => false,
+	    'capability_type'	 => 'post',
+	    'capabilities'		 => array(
+		'create_posts' => false, // Removes support for the "Add New" function
+	    ),
+	    'map_meta_cap'		 => true,
+	);
 
-        register_post_type('braintree_payment', $args);
+	register_post_type( 'braintree_payment', $args );
     }
 
-    static function insert_post($data) {
-        $post = array();
-        $post['post_title'] = sanitize_text_field($data['item_name'] . ' - ' . $data['amount'].', '.__('paid by ','wp_braintree_lang').$data['name'].' ('.$data['email'].') ');
-        $post['post_status'] = 'publish';
+    static function insert_post( $data ) {
+	$post			 = array();
+	$post[ 'post_title' ]	 = sanitize_text_field( $data[ 'item_name' ] . ' - ' . $data[ 'amount' ] . ', ' . __( 'paid by ', 'wp_braintree_lang' ) . $data[ 'name' ] . ' (' . $data[ 'email' ] . ') ' );
+	$post[ 'post_status' ]	 = 'publish';
 
-        $output = '';
-        $output .= __("<h4>Payment Details</h4>", "wp_braintree_lang") . "\n";
-        $output .= __("Payment Time: ", "wp_braintree_lang") . date("F j, Y, g:i a", $data['date']) . "\n";
-        $output .= __("Transaction ID: ", "wp_braintree_lang") . $data['trans_id'] . "\n";
-        $output .= __("Order ID: ", "wp_braintree_lang") . $data['order_id'] . "\n";
+	$output	 = '';
+	$output	 .= __( "<h4>Payment Details</h4>", "wp_braintree_lang" ) . "\n";
+	$output	 .= __( "Payment Time: ", "wp_braintree_lang" ) . date( "F j, Y, g:i a", $data[ 'date' ] ) . "\n";
+	$output	 .= __( "Transaction ID: ", "wp_braintree_lang" ) . $data[ 'trans_id' ] . "\n";
+	$output	 .= __( "Order ID: ", "wp_braintree_lang" ) . $data[ 'order_id' ] . "\n";
 //        $output .= __("Description: ", "wp_braintree_lang") . $order_details['charge_description'] . "\n";
-        $output .= "--------------------------------" . "\n";
-        $output .= __("Item Name: ", "wp_braintree_lang") . $data['item_name'] . "\n";
-        $output .= __("Amount Paid: ", "wp_braintree_lang") . $data['amount'] . "\n";
-        $output .= "--------------------------------" . "\n";
+	$output	 .= "--------------------------------" . "\n";
+	$output	 .= __( "Item Name: ", "wp_braintree_lang" ) . $data[ 'item_name' ] . "\n";
+	$output	 .= __( "Amount Paid: ", "wp_braintree_lang" ) . $data[ 'amount' ] . "\n";
+	$output	 .= "--------------------------------" . "\n";
 
-        $output .= "\n";
+	$output .= "\n";
 
-        $output .= __("<h4>Customer Details</h4>", "wp_braintree_lang") . "\n";
-        $output .= __("Name: ", "") . $data['name'] . "\n";
-        $output .= __("E-Mail Address: ", "wp_braintree_lang") . $data['email'] . "\n";
+	$output	 .= __( "<h4>Customer Details</h4>", "wp_braintree_lang" ) . "\n";
+	$output	 .= __( "Name: ", "" ) . $data[ 'name' ] . "\n";
+	$output	 .= __( "E-Mail Address: ", "wp_braintree_lang" ) . $data[ 'email' ] . "\n";
 
-        $post['post_content'] = $output;
-        $post['post_type'] = 'braintree_payment';
-        
-        return wp_insert_post($post); //Return post ID
+	$post[ 'post_content' ]	 = $output;
+	$post[ 'post_type' ]	 = 'braintree_payment';
+
+	return wp_insert_post( $post ); //Return post ID
     }
 
 }
--- wp-braintree/js/front_page.js
+++ wp-braintree/js/front_page.js
@@ -1,108 +1,108 @@
 jQuery(document).ready(function ($) {
 
     function wp_braintree_create_instance(id) {
-        if (typeof braintree != 'undefined') {
+	if (typeof braintree != 'undefined') {
 
-            wp_braintree_buttons[id].handler = braintree.client.create({
-                authorization: window['wp_braintree_buttons_data_' + id].client_token,
-            }, function (err, clientInstance) {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                braintree.hostedFields.create({
-                    client: clientInstance,
-                    styles: {
-                        'input': {
-                            'font-size': '14px',
-                            'font-family': 'helvetica, tahoma, calibri, sans-serif',
-                            'color': '#3a3a3a',
-                        },
-                        ':focus': {
-                            'color': 'black'
-                        }
-                    },
-                    fields: {
-                        number: {
-                            selector: '#wp-braintree-card-number-' + id,
-                            placeholder: '4111 1111 1111 1111'
-                        },
-                        cvv: {
-                            selector: '#wp-braintree-cvv-' + id,
-                            placeholder: '123'
-                        },
-                        expirationMonth: {
-                            selector: '#wp-braintree-expiration-month-' + id,
-                            placeholder: 'MM'
-                        },
-                        expirationYear: {
-                            selector: '#wp-braintree-expiration-year-' + id,
-                            placeholder: 'YY'
-                        },
-                    }
-                }, function (err, hostedFieldsInstance) {
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
-                    jQuery('#braintree-payment-form-' + id).submit(function (event) {
-                        if (!wp_braintree_buttons[id].tokenizeSuccess)
-                            event.preventDefault();
-                        else {
-                            wp_braintree_buttons[id].tokenizeSuccess = false;
-                            return true;
-                        }
-                        hostedFieldsInstance.tokenize(function (err, payload) {
-                            if (err) {
-                                wpbErrorMsg = '';
-                                if (typeof err.details != "undefined") {
-                                    var firstInvalidField = err.details.invalidFieldKeys[0];
-                                    switch (firstInvalidField) {
-                                        case "number":
-                                            wpbErrorMsg = wp_braintree_scripts_front_js_vars.cc_no_valid;
-                                            break;
-                                        case "cvv":
-                                            wpbErrorMsg = wp_braintree_scripts_front_js_vars.cvv_number;
-                                            break;
-                                        case "expirationMonth":
-                                            wpbErrorMsg = wp_braintree_scripts_front_js_vars.exp_month_number;
-                                            break;
-                                        case "expirationYear":
-                                            wpbErrorMsg = wp_braintree_scripts_front_js_vars.exp_year_number;
-                                            break;
-                                        default:
-                                            wpbErrorMsg = wp_braintree_scripts_front_js_vars.check_fields;
-                                    }
+	    wp_braintree_buttons[id].handler = braintree.client.create({
+		authorization: window['wp_braintree_buttons_data_' + id].client_token,
+	    }, function (err, clientInstance) {
+		if (err) {
+		    console.error(err);
+		    return;
+		}
+		braintree.hostedFields.create({
+		    client: clientInstance,
+		    styles: {
+			'input': {
+			    'font-size': '14px',
+			    'font-family': 'helvetica, tahoma, calibri, sans-serif',
+			    'color': '#3a3a3a',
+			},
+			':focus': {
+			    'color': 'black'
+			}
+		    },
+		    fields: {
+			number: {
+			    selector: '#wp-braintree-card-number-' + id,
+			    placeholder: '4111 1111 1111 1111'
+			},
+			cvv: {
+			    selector: '#wp-braintree-cvv-' + id,
+			    placeholder: '123'
+			},
+			expirationMonth: {
+			    selector: '#wp-braintree-expiration-month-' + id,
+			    placeholder: 'MM'
+			},
+			expirationYear: {
+			    selector: '#wp-braintree-expiration-year-' + id,
+			    placeholder: 'YY'
+			},
+		    }
+		}, function (err, hostedFieldsInstance) {
+		    if (err) {
+			console.error(err);
+			return;
+		    }
+		    jQuery('#braintree-payment-form-' + id).submit(function (event) {
+			if (!wp_braintree_buttons[id].tokenizeSuccess)
+			    event.preventDefault();
+			else {
+			    wp_braintree_buttons[id].tokenizeSuccess = false;
+			    return true;
+			}
+			hostedFieldsInstance.tokenize(function (err, payload) {
+			    if (err) {
+				wpbErrorMsg = '';
+				if (typeof err.details != "undefined") {
+				    var firstInvalidField = err.details.invalidFieldKeys[0];
+				    switch (firstInvalidField) {
+					case "number":
+					    wpbErrorMsg = wp_braintree_scripts_front_js_vars.cc_no_valid;
+					    break;
+					case "cvv":
+					    wpbErrorMsg = wp_braintree_scripts_front_js_vars.cvv_number;
+					    break;
+					case "expirationMonth":
+					    wpbErrorMsg = wp_braintree_scripts_front_js_vars.exp_month_number;
+					    break;
+					case "expirationYear":
+					    wpbErrorMsg = wp_braintree_scripts_front_js_vars.exp_year_number;
+					    break;
+					default:
+					    wpbErrorMsg = wp_braintree_scripts_front_js_vars.check_fields;
+				    }
 
-                                } else {
-                                    wpbErrorMsg = wp_braintree_scripts_front_js_vars.fill_fields;
-                                }
-                                if (wpbErrorMsg != '') {
-                                    jQuery("<div>" + wpbErrorMsg + "</div>").dialog({
-                                        title: wp_braintree_scripts_front_js_vars.val_errors,
-                                        width: 'auto',
-                                        maxWidth: 600,
-                                        height: 'auto',
-                                        modal: true,
-                                        fluid: true,
-                                        buttons: {
-                                            Ok: function () {
-                                                jQuery(this).dialog("close");
-                                            }
-                                        }
-                                    });
-                                }
-                            } else {
-                                jQuery('#wp-braintree-nonce-' + id).val(payload.nonce);
-                                wp_braintree_buttons[id].tokenizeSuccess = true;
-                                jQuery('button[data-wp-braintree-button-id="' + id + '"]').prop('disabled', true);
-                                jQuery('#braintree-payment-form-' + id).submit();
-                            }
-                        });
-                    });
-                });
-            });
-        }
+				} else {
+				    wpbErrorMsg = wp_braintree_scripts_front_js_vars.fill_fields;
+				}
+				if (wpbErrorMsg != '') {
+				    jQuery("<div>" + wpbErrorMsg + "</div>").dialog({
+					title: wp_braintree_scripts_front_js_vars.val_errors,
+					width: 'auto',
+					maxWidth: 600,
+					height: 'auto',
+					modal: true,
+					fluid: true,
+					buttons: {
+					    Ok: function () {
+						jQuery(this).dialog("close");
+					    }
+					}
+				    });
+				}
+			    } else {
+				jQuery('#wp-braintree-nonce-' + id).val(payload.nonce);
+				wp_braintree_buttons[id].tokenizeSuccess = true;
+				jQuery('button[data-wp-braintree-button-id="' + id + '"]').prop('disabled', true);
+				jQuery('#braintree-payment-form-' + id).submit();
+			    }
+			});
+		    });
+		});
+	    });
+	}
     }
 
     // Hide ALL forms on page load
@@ -112,11 +112,11 @@ jQuery(document).ready(function ($) {
     // When a "buy now" button is clicked
     $('.submit_buy_now').click(function (e) {
 
-        e.preventDefault();
-        id = $(this).attr('data-wp-braintree-button-id');
-        //wp_braintree_create_instance(id);
-        $('.dialog-form[data-wp-braintree-button-id="' + id + '"]').slideDown();
-        $(this).hide();
+	e.preventDefault();
+	id = $(this).attr('data-wp-braintree-button-id');
+	//wp_braintree_create_instance(id);
+	$('.dialog-form[data-wp-braintree-button-id="' + id + '"]').slideDown();
+	$(this).hide();
     });
 
     // Variables used for BrainTree API
@@ -125,40 +125,40 @@ jQuery(document).ready(function ($) {
 
     // This is used when the page is submitted; it displays the transaction result
     if (typeof $("#dialog-message-success").dialog === 'function') {
-        $("#dialog-message-success").dialog({// If payment was success; this is the message just before the redirect url
-            width: 'auto',
-            maxWidth: 600,
-            height: 'auto',
-            modal: true,
-            fluid: true,
-            buttons: {
-                Ok: function () {
-                    $(this).dialog("close");
-                }
-            },
-            close: function (event, ui) {
-                $(this).remove();
-                // Redirect after alert message
-                window.location = wp_braintree_scripts_front_js_vars.success_url;
-            }
-        });
+	$("#dialog-message-success").dialog({// If payment was success; this is the message just before the redirect url
+	    width: 'auto',
+	    maxWidth: 600,
+	    height: 'auto',
+	    modal: true,
+	    fluid: true,
+	    buttons: {
+		Ok: function () {
+		    $(this).dialog("close");
+		}
+	    },
+	    close: function (event, ui) {
+		$(this).remove();
+		// Redirect after alert message
+		window.location = wp_braintree_scripts_front_js_vars.success_url;
+	    }
+	});
     }
     if (typeof $("#dialog-message-error").dialog === 'function') {
-        $("#dialog-message-error").dialog({// If payment failed; alert message with only option to go back and fix form to resubmit
-            width: 'auto',
-            maxWidth: 600,
-            height: 'auto',
-            modal: true,
-            fluid: true,
-            buttons: {
-                Back: function () {
-                    history.back();
-                }
-            },
-            close: function (event, ui) {
-                history.back();
-            }
-        });
+	$("#dialog-message-error").dialog({// If payment failed; alert message with only option to go back and fix form to resubmit
+	    width: 'auto',
+	    maxWidth: 600,
+	    height: 'auto',
+	    modal: true,
+	    fluid: true,
+	    buttons: {
+		Back: function () {
+		    history.back();
+		}
+	    },
+	    close: function (event, ui) {
+		history.back();
+	    }
+	});
     }
     // Before we actually submit any form for transaction... we want to check the input fields.
     // This will help since each time the form is submitted.. all fields are cleared.
@@ -167,9 +167,9 @@ jQuery(document).ready(function ($) {
     var wp_braintree_buttons = [];
 
     $('form[data-wp-braintree-button-id]').each(function (id) {
-        wp_braintree_buttons[id] = [];
-        wp_braintree_buttons[id].tokenizeSuccess = false;
-        wp_braintree_create_instance(id);
+	wp_braintree_buttons[id] = [];
+	wp_braintree_buttons[id].tokenizeSuccess = false;
+	wp_braintree_create_instance(id);
     });
 
 });
